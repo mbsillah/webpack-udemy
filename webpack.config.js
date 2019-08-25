2719f0a0372b87bfd42f312a5de03924920f2b26
sillah2010 <macktastic2014@gmail.com>
2019-08-24 20:00:16 -0400

    Finished loaders sections of udemy

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,19 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['transform-class-properties']
+                    }
+                }
             }
         ]
     }
