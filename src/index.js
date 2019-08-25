1cb497feb041dd5597747ee6cd0656055f7b56e7
sillah2010 <macktastic2014@gmail.com>
2019-08-24 20:57:34 -0400

    Started plugins sections of udemy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import HelloWorldButton from './components/hello-world-button/helloWorldButton';
+import Heading from './components/heading/heading';
 import addImage from './addImage';
 
+const heading = new Heading();
 const hellowWorldButton = new HelloWorldButton();
+
+heading.render();
 hellowWorldButton.render();
 
 addImage();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -16,11 +18,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.js$/,
@@ -34,5 +36,11 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
 }
