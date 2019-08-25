36558a315d5a9d6ef6b4dec802ca5956b5212a05
sillah2010 <macktastic2014@gmail.com>
2019-08-24 22:20:30 -0400

    Started dev/prod sections of udemy

---
+++ webpack.dev.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './build'),
+        publicPath: ''
+    },
+    mode: 'development',
+    module: {
+        rules: [    
+            {
+                test: /\.(png|jpg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['transform-class-properties']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "DIO's World"
+        })
+    ]
+}
